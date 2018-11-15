e303eec5e289d981a2bcd8d5829425370aeda4a9
boyur <gboyur@gmail.com>
2018-11-15 23:03:41 +0300

    add out to gitignore

--- out/_next/static/bGLmP6~Ia9zB7IxknbWMF/pages/projects.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{240:function(e,n,t){__NEXT_REGISTER_PAGE("/projects",function(){return e.exports=t(241),{page:e.exports.default}})},241:function(e,n,t){"use strict";t.r(n);var r=t(0),o=t.n(r),u=t(32);n.default=function(){return o.a.createElement(u.a,null,o.a.createElement("div",null,"projects"))}}},[[240,1,0]]]);
