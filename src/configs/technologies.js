0735e9c91829e2f3a936c01f1336bcfe99052de3
boyur <gboyur@gmail.com>
2018-11-20 22:01:30 +0300

    fix image urls

--- src/configs/technologies.js
+++ src/configs/technologies.js
@@ -3,18 +3,18 @@ export default [
     title: 'galton',
     description: 'development',
     url: 'https://github.com/urbica/galton',
-    img: './images/technologies/galton.jpg',
+    img: '/images/technologies/galton.jpg',
   },
   {
     title: 'martin',
     description: 'development',
     url: 'https://martin.urbica.co/',
-    img: './images/technologies/martin.jpg',
+    img: '/images/technologies/martin.jpg',
   },
   {
     title: 'REACT MAP GL',
     description: 'development',
     url: 'https://github.com/urbica/react-map-gl',
-    img: './images/technologies/map-gl.png',
+    img: '/images/technologies/map-gl.png',
   },
 ];
