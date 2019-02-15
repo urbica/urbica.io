e202ebe8e80d5f30dd7f3b6778df0a9372c06cf6
boyur <gboyur@gmail.com>
2019-02-15 15:26:37 +0300

    add favicon

--- src/html.js
+++ src/html.js
@@ -13,13 +13,28 @@ export default class HTML extends React.Component {
           name="viewport"
           content="width=device-width, initial-scale=0.7, shrink-to-fit=no"
         />
-        <meta property="og:url" content="http://urbica.co" />
-        <meta property="og:title" content="Urbica" />
-        <meta property="og:description" content="We design data tools for getting is insights and making decisions" />
-        <meta property="og:image" content="https://urbica.co/fb.jpg" />
+        <meta property="og:url" content="http://urbica.co"/>
+        <meta property="og:title" content="Urbica"/>
+        <meta property="og:description" content="We design data tools for getting is insights and making decisions"/>
+        <meta property="og:image" content="https://urbica.co/fb.jpg"/>
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicon/manifest.json"/>
 
         <title>Urbica</title>
-        <meta name="description" content="We design data tools for getting is insights and making decisions" />
+        <meta name="description" content="We design data tools for getting is insights and making decisions"/>
         {this.props.headComponents}
       </head>
       <body {...this.props.bodyAttributes}>
@@ -31,8 +46,8 @@ export default class HTML extends React.Component {
       />
       {this.props.postBodyComponents}
       </body>
-    </html>
-  )
+      </html>
+    )
   }
 }
 
